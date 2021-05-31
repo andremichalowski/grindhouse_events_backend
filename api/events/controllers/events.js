29a068e78a8d06a809d356b0e1be50ddfbe0913b
amichalo421 <andre.michalowski@gmail.com>
2021-05-31 17:12:39 -0400

    Role for 'me' (data based on user.id)

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,8 +1,31 @@
 'use strict';
+const { default: createStrapi } = require('strapi');
+const { sanitizeEntity } =require('strapi-utils')
+
+// const { default: createStrapi } = require("strapi");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // Get logged in users
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    if(!user) {
+      return ctx.badRequest(null, [
+        { message: [{ id: "No authorization header has been called."}]}
+      ]);
+    }
+
+    const data = await createStrapi.services.events.find({user: user.id})
+
+    if(!data) {
+      return ctx.notFound()
+    }
+
+    return sanitizeEntity(data, {model: createStrapi.models.events})
+  }
+};
